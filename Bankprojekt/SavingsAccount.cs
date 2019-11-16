55ec095659aa09e449e42c9e1c16c50dc7865f95
Tim Segerstéen <tim.segersteen@live.se>
2019-11-16 02:25:42 +0100

    lagt till den försvunna klassen savings account

---
+++ Bankprojekt/SavingsAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankprojekt
+{
+    
+    public class SavingsAccount
+    {
+        public decimal Saldo { get; set; }
+        public double Räntesats { get; set; }
+        public string Kontotyp { get; set; }
+        public int AccountId { get; set; }
+    }
+
+}
+
