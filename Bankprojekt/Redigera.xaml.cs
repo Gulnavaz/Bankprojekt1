dd55bf52ab6cbe64b335f1636fc7f0da06c5ce93
Gulnavaz <gulnavazwali1@hotmail.com>
2019-11-18 09:41:24 +0100

    fixat list

--- Bankprojekt/Redigera.xaml.cs
+++ Bankprojekt/Redigera.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -23,19 +24,15 @@ namespace Bankprojekt
     public sealed partial class Redigera : Page
     {
         public Customer Selected { get; set; }
+        private ObservableCollection<Customer> customerslist = new ObservableCollection<Customer>();
+        ObservableCollection<Customer> CustList { get { return customerslist; } }
         public Redigera()
         {
             this.InitializeComponent();
-            App.Customers.Add(new Customer() { Name = "Björn Johansson", PNr = 8912022738 });
-            App.Customers.Add(new Customer() { Name = "Fredrik Karlsson", PNr = 9202131242 });
-            App.Customers.Add(new Customer() { Name = "Amanda Andersson", PNr = 9412038234 });
-            App.Customers.Add(new Customer() { Name = "Alexander Johansson", PNr = 8205021523 });
-            App.Customers.Add(new Customer() { Name = "Alex Larsen", PNr = 6305151526 });
-            App.Customers.Add(new Customer() { Name = "Anders Larsson", PNr = 6908172592 });
-            App.Customers.Add(new Customer() { Name = "Anders Ohlsen", PNr = 7811022849 });
-            App.Customers.Add(new Customer() { Name = "Pontus Fyhrberg", PNr = 7412199572 });
-
-            CustomersListView.ItemsSource = App.Customers.OrderBy(x => x.Name);
+            foreach(Customer cust in App.Customers)
+            {
+                customerslist.Add(cust);
+            }
             //CustomersAccountListView.ItemsSource = Selected.Accounts;
         }
 
